055f411b9eaae624179b56d6a0ffa05f841dfe31
sowmyashree <sowmyashreem@microsoft.com>
2024-12-12 12:09:08 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Main script for the local action in the other repository
+
+// Add your code here
+console.log("Hello from the local action!");
+
+// You can define your action logic here
+function run() {
+  console.log("Running the local action");
+  // Add your logic here
+}
+
+// Export the run function as the entry point for the action
+module.exports = run;
